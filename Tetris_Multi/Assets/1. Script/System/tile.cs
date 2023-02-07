8b403df99e85c75f871e67626c4854aefcca4079
kyhyum <dudrhkd0225@naver.com>
2023-02-07 15:41:13 +0900

    Multi System complete

--- Tetris_Multi/Assets/1. Script/System/tile.cs
+++ Tetris_Multi/Assets/1. Script/System/tile.cs
@@ -11,6 +11,7 @@ public class tile : MonoBehaviourPun
     //public TextMeshProUGUI _123;
     //public TextMeshProUGUI remote_123;
 
+    public Tile createtile;
     public GameObject board;
     public Tilemap tilemap { get; private set; }
     private Tilemap remote_tile;
@@ -29,19 +30,19 @@ public class tile : MonoBehaviourPun
     {
         this.remote_tile = GetComponentInChildren<Tilemap>();
         tilemap = board.GetComponentInChildren<Tilemap>();
+        InvokeRepeating("hastile", 0f, 0.1f);
         // this.remote_tilemap.color = Color.black;
     }
     private void Update()
     {
-        hastile();
     }
 
     public void hastile()
     {
-        /*if (!PhotonNetwork.IsConnected)
-             return;*/
         RectInt bounds = this.Bounds;
         int row = bounds.yMin;
+        int clearrow = bounds.yMin;
+
         while (row < bounds.yMax)
         {
             for (int col = bounds.xMin; col < bounds.xMax; col++)
@@ -50,41 +51,31 @@ public class tile : MonoBehaviourPun
 
                 if (tilemap.HasTile(pos))
                 {
-                    Debug.Log("aa");
                     TileBase above = this.tilemap.GetTile(pos);
-                    photonView.RPC("createblock", RpcTarget.Others, (Vector3)pos);
+                    photonView.RPC("createblock", RpcTarget.Others, col, row, true);
                 }
                 else
-                    this.remote_tile.SetTile(pos, null);
+                    photonView.RPC("createblock", RpcTarget.Others, col, row, false);
             }
             row++;
-
         }
     }
 
     [PunRPC]
-    void createblock(Vector3 pos)
+    void createblock(int col, int row, bool a)
     {
-        Vector3Int position = new Vector3Int((int)pos.x, (int)pos.y, 0);
-        TileBase above = this.tilemap.GetTile(position);
-        this.remote_tile.SetTile(position, above);
-        remote_tile.color = Color.black;
+        if (a)
+        {
+            Vector3Int pos = new Vector3Int(col, row, 0);
+            this.remote_tile.SetTile(pos, createtile);
+            this.remote_tile.color = Color.grey;
+            Color color = this.remote_tile.color;
+            color.a = 0.1f;
+        }
+        else
+        {
+            Vector3Int pos = new Vector3Int(col, row, 0);
+            this.remote_tile.SetTile(pos, null);
+        }
     }
-
-    /* public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-     {
-         if (stream.IsWriting)
-         {
-             stream.SendNext(_123.text);
-
-         }
-         else
-         {
-             remote_123.text = (string)stream.ReceiveNext();
-         }
-     }
-     public void aa()
-     {
-         _123.text = Random.Range(1, 10000).ToString();
-     }*/
 }
